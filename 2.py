0dea9deea4425724097561f9a028c89d0ffd80f8
mziyrk <maziyary10@gmail.com>
2022-07-08 13:29:03 +0430

    ezaf shod

---
+++ 2.py
@@ -0,0 +1,77 @@
+ s = "python"                                 #reshte
+s2 = "123"
+print(s [0:3])
+print(s2 [:2])
+print(s [:])
+length = len (s)
+print (length)
+print (s [length - 1])
+print (s [length - 4])
+
+name.upper()               #METOD
+name.lower()
+name.count("X")
+name.find("X")
+
+
+d = ["python",158,["47574"],"maziyar",0214,555]            #list
+print(d [1])
+print(d [-1])
+print (d [0][1])
+print(d [-1][-2])
+length = len (d)
+print(length )
+
+name.append(X)              #METOD
+name.remove(X)
+name.index(x)
+name.count(x)
+name.extend(x)
+name.insert(i,x)
+name.sort()
+
+t = "maziyar",125,545,"hdbfv",s44             #tuple
+t2 = (fk55,555,"fdsjgm",444)
+type(t2)
+print(t [0])
+print(t2 [-2])
+
+name.index(x)                #METOD
+name.count(x)
+
+
+
+
+
+g = {"math":19,"physics":14,"arabic":20}    #dictionary
+print(g["math"])
+
+name.copy()
+name.clear()             #METOD
+name.has_key()
+name.keys()
+name.values()
+name.items()
+name.get()
+
+
+s = {2,3,5,"KLKL","YYY"}                #set
+
+
+name.isdisjoint(x)
+NAME.ISSUBSET(x)        #METOD
+name.issuperset(x)
+name.union(x)
+name.intersection(x)
+name.difference(x)
+name.symmetric_difference(x)
+
+name.copy()
+name.clear()
+name.add(x)
+name.remove(x)
+name.discard(x)
+name.pop()
+name.update(x)
+
+
